0d38d561a5a7161bf60bf212885c3e58d4359ae4
venkatesh-gitme <venkateshrai233@gmail.com>
2026-02-10 16:50:23 +0530

    Update Q4.cpp

--- Assignment 3/Q4.cpp
+++ Assignment 3/Q4.cpp
@@ -1,1 +1,41 @@
 //4. Write a program to demonstrate the working of friend class.
+
+#include <iostream>
+using namespace std;
+
+class B;
+
+class A {
+    int x;
+
+public:
+    A(int x) {
+        this->x = x;
+    }
+
+    friend class B;
+
+    void show() {
+        cout << "A = " << x << endl;
+    }
+};
+
+class B {
+public:
+    void display(A a) {
+        cout << "Accessing private data of A from B: " << a.x << endl;
+    }
+};
+
+int main() {
+    A objA(50);
+    B objB;
+
+    cout << "From class A:" << endl;
+    objA.show();
+
+    cout << endl << "From friend class B:" << endl;
+    objB.display(objA);
+
+    return 0;
+}
