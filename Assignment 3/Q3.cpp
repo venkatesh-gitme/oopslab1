f022508a14f0c1d07188305a53c2e055accb2dea
venkatesh-gitme <venkateshrai233@gmail.com>
2026-02-10 16:46:51 +0530

    Update Q3.cpp

--- Assignment 3/Q3.cpp
+++ Assignment 3/Q3.cpp
@@ -1,2 +1,55 @@
 // 3. Write a program to add data objects of two different classes using friend functions.
 
+#include <iostream>
+using namespace std;
+
+class B;
+
+class A {
+    int x;
+
+public:
+    A(int x) {
+        this->x = x;
+    }
+
+    friend int addValues(A, B);
+
+    void show() {
+        cout << "A = " << x << endl;
+    }
+};
+
+class B {
+    int y;
+
+public:
+    B(int y) {
+        this->y = y;
+    }
+
+    friend int addValues(A, B);
+
+    void show() {
+        cout << "B = " << y << endl;
+    }
+};
+
+int addValues(A a, B b) {
+    return a.x + b.y;
+}
+
+int main() {
+    A objA(15);
+    B objB(25);
+
+    cout << "Object values:" << endl;
+    objA.show();
+    objB.show();
+
+    int sum = addValues(objA, objB);
+
+    cout << endl << "Sum = " << sum << endl;
+
+    return 0;
+}
