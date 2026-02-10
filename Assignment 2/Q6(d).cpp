0fc137d7d5fe8f6c3df1dbd4ebedb78e84fa74fa
venkatesh-gitme <venkateshrai233@gmail.com>
2026-02-10 16:32:32 +0530

    Create Q6(d).cpp

---
+++ Assignment 2/Q6(d).cpp
@@ -0,0 +1,8 @@
+// 6. Implement scope resolution operator : : for the following uses:
+// (d) Use inbuilt libraries (cin cout with scope resolution operator)
+#include<iostream>
+int main()
+{
+std::cout<<"Using cout with scope resolution operator";
+return 0;
+}
