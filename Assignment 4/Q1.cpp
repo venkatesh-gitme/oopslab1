687312cd816ff9de07b1181a487f7658bc4e6c17
venkatesh-gitme <venkateshrai233@gmail.com>
2026-04-07 16:03:23 +0530

    Update and rename Q1 to Q1.cpp

---
+++ Assignment 4/Q1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class rect{
+    int l,b;
+public:
+    rect(){
+        l=0;
+        b=0;
+    }
+    rect(int x){
+        l=x;
+        b=x;
+    }
+    rect(int x,int y){
+        l=x;
+        b=y;
+    }
+    int area(){
+        return l*b;
+    }
+};
+
+int main(){
+    cout<<"Deepak Kumar(1024150071)"<<endl;
+    rect r1;
+    rect r2(7);        
+    rect r3(3,8);      
+
+    cout<<"area (no value): "<<r1.area()<<endl;
+    cout<<"area (one value): "<<r2.area()<<endl;
+    cout<<"area (two values): "<<r3.area()<<endl;
+
+    return 0;
+}
